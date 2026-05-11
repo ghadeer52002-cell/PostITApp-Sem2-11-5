6666ebc449bb265106052e8a1483ffa80bd6ab68
ghadeer52002-cell <ghadeer52002@gmail.com>
2026-05-11 15:31:02 +0400

    Update UserSlice.js

--- client/src/features/UserSlice.js
+++ client/src/features/UserSlice.js
@@ -11,7 +11,7 @@ const initialState={
  
 export const addUser=createAsyncThunk("user/addUser",async(userData)=>{
     try{
-        const response=await axios.post("http://localhost:3002/register",userData);
+        const response=await axios.post("https://postitapp-sem2-11-5.onrender.com/register",userData);
         return response.data.message;
     }
     catch(error){
@@ -21,7 +21,7 @@ export const addUser=createAsyncThunk("user/addUser",async(userData)=>{
 
 export const login=createAsyncThunk("user/login",async(userData)=>{
     try{
-        const response=await axios.post("http://localhost:3002/login",userData);
+        const response=await axios.post("https://postitapp-sem2-11-5.onrender.com/login",userData);
         return response.data;
     }
     catch(error){
